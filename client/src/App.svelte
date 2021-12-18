f72e34d3f089f87684e3dc5eafe5eb5eea384c96
Peter-Evans-Cerberus <peter.evans@cerberusnuclear.com>
2021-12-18 15:22:16 +0000

    flask is now serving svelte app

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
-	export let name: string;
+
+	let data:boolean = false; 
+	let value:string = '';
+
+	function getCurrencyData() {
+		console.log("fetching...");
+		fetch("./get_data")
+		.then(Response => {
+			value = Response.toString();
+			data = true;
+			})
+			.catch(error => {
+				console.log(error);
+			});
+  	}
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Heading</h1>
+	<button on:click={getCurrencyData}>Get data</button>
+	{#if data}
+	<div id="data">
+		<p>{value}</p>
+	</div>
+	{/if}
 </main>
 
 <style>
@@ -13,13 +33,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
--- server.py
+++ server.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 import random
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "Hello, world!"
+    #return "<h1>Hello from Flask!</h1>"
+    return send_from_directory('client/public', 'index.html')
+
+
+
+# Path for all the static files (compiled JS/CSS, etc.)
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory('client/public', path)
+
+
+
+@app.route("/get_data")
+def get_data():
+    # call currency data api from site like fixer.io
+    return "return from api"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
